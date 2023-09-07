2e3d822d40cd006d92647323c3f7b198bbf1b034
tech-gecko <w0nderzz2001@gmail.com>
2023-09-07 15:16:13 -0700

    Update3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-	write(STDERR, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fwrite(STDERR, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
 
