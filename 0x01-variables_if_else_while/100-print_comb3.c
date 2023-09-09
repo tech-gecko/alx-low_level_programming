bfaaabed7209f708152fc65ccf675772c046ce40
tech-gecko <w0nderzz2001@gmail.com>
2023-09-08 21:29:34 -0700

    Update1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (a == 8 && b == 9)
+				break;
+			putchar(',');
+			putchat(' ');
+		}
+	}
+	putchar('\n');
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j 
