cd04166ef6ce3f8319c83d5c7e9bac296a76cf5d
tech-gecko <w0nderzz2001@gmail.com>
2023-09-28 06:53:07 -0700

    Test with Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Recursively prints all characters of a string.
+ * @s: Pointer-to-string whose characters are to be printed.
+ *
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else if (*s == 0)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
+
