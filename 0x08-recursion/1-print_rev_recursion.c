b29232e788e92890c9a89ab645ad2b7f151f8ff1
tech-gecko <w0nderzz2001@gmail.com>
2023-09-28 08:47:02 -0700

    Update4

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,16 +8,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-	char *p = s;
-	p = s[sizeof(s) - 2];
-
-	if (*p = s[0])
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*p);
-	_print_rev_recursion(s - 1);
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
 
 
