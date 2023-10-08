b462a799177a1ae4a92eae2ef19c306841b560a6
tech-gecko <w0nderzz2001@gmail.com>
2023-10-08 09:42:17 -0700

    Update33

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = (int **)malloc(sizeof(int) * (height * width));
+	p = (int **)malloc(sizeof(int) * height);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
