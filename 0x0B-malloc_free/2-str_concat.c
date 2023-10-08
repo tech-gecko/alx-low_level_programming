9c19dd6c6c59f046a3c0dfeb7c59240f60e44008
tech-gecko <w0nderzz2001@gmail.com>
2023-10-07 19:19:41 -0700

    Update19

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,13 +10,13 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int l1, l2;
+	char *ps1s2 = s1;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	int l1, l2;
-	char *ps1s2 = s1;
-
 	while (s1[l1] != '\0')
 		l1++;
 	while (s2[l2] != '\0')
