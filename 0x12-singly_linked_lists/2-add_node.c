b70f15138e6cec9a9be8603ca321da67c837e0d1
tech-gecko <w0nderzz2001@gmail.com>
2023-10-19 19:02:16 -0700

    Update Task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head __attribute((unused))__, const char *str)
+{
+	list_t *new;
+	size_t i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	else
+	{
+		new->str = strdup(str);
+		while (str[i] != '\0')
+			i++;
+
+		new->len = i;
+		new->next = *head;
+	}
+	return (new);
+	free(new);
+	
+}
