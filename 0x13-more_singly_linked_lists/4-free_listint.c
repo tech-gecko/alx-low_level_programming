e51ce4e82fd59cd323edbe72f36bd73b7ffdeab5
tech-gecko <w0nderzz2001@gmail.com>
2023-10-25 08:59:25 -0700

    Update 1 Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a listint_t list.
+ * @head: First member of the listint_t list.
+ *
+ * Return: Nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->n);
+		free(temp);
+	}
+}
