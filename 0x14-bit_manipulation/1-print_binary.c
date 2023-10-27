4d1c02e4c673fe9720f7be45be2b2e12f115f38b
tech-gecko <w0nderzz2001@gmail.com>
2023-10-26 19:59:40 -0700

    Update 2 Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,16 +8,17 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << 31;
+	int i = sizeof(unsigned long int) * 8 - 1, mask = 0;
 
-	while (mask != 0)
+	while (i >= 0)
 	{
-		if ((n & mask) != 0)
+		if (n & (1UL << i) != 0)
+		{
 			_putchar('1');
-		else
+			mask = 1;
+		}
+		else if (mask == 1)
 			_putchar('0');
-
-		mask >>= 1;
 	}
 	_putchar('\n');
 }
