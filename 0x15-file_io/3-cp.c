423d659017bfd031218ed25beb8965972086e2ae
tech-gecko <w0nderzz2001@gmail.com>
2023-10-31 06:52:51 -0700

    Update 1, Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,80 @@
+#include "main.h"
+
+/**
+ * error - Easily handles errors.
+ * @code: Exit code.
+ * @format: Error message.
+ * @arg: Argumenet related to error.
+ *
+ * Return: Nothing.
+*/
+void error(int code, const char *format, const char *arg)
+{
+	dprintf(STDERR_FILENO, format, arg);
+	exit(code);
+}
+
+/**
+ * errorFD - Easily handle errors for FDs.
+ * @code: Exit code.
+ * @format: Error message.
+ * @FD: file descriptor value related to error.
+ *
+ * Return: Nothing.
+*/
+void errorFD(int code, const char *format, int FD)
+{
+	dprintf(STDERR_FILENO, format, FD);
+	exit(code);
+}
+
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: 0 if success.
+*/
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t bytesRead, bytesWritten;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		error(97, "Usage: cp file_from file_to\n");
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		error(98, "Error: Can't read from file %s\n", argv[1]);
+	}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		error(99, "Error: Can't write to file %s\n", argv[2]);
+	}
+	while ((bytesRead = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytesWritten = write(fd_to, buffer, bytesRead);
+		if (bytesWritten != bytesRead)
+		{
+			error(99, "Error: Can't write to file %s\n", argv[2]);
+		}
+	}
+	if (bytesRead == -1)
+	{
+		error(98, "Error: Can't read from file %s\n", argv[1]);
+	}
+	if (close(fd_from) == -1)
+	{
+		errorFD(100, "Error: Can't close fd %d\n", fd_from);
+	}
+	if (close(fd_to) == -1)
+	{
+		errorFD(100, "Error: Can't close fd %d\n", fd_to);
+	}
+	return (0);
+}
