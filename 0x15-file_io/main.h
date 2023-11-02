de64d0745cb23b56609e445bed8755f428d073f1
tech-gecko <w0nderzz2001@gmail.com>
2023-11-02 10:25:16 -0700

    Update final tweak

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,12 +9,18 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <elf.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+#define _POSIX_C_SOURCE 200809L
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char *argv[]);
-
+void error(int code, const char *format, const char *arg);
+void errorFD(int code, const char *format, int FD);
 
 #endif
