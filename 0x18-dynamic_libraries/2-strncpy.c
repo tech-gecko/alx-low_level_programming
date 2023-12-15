9610c3e6aa89306e41442c8a608f683f188f69a0
tech-gecko <w0nderzz2001@gmail.com>
2023-12-15 13:49:15 -0800

    2

--- 0x18-dynamic_libraries/2-strncpy.c
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	char *val = dest;
 	int i = 0;
 
-	while (i < n &&)
+	while (i < n)
 	{
 		if (*src != '\0')
 		{
--- 0x18-dynamic_libraries/3-strcmp.c
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (*s1[i] == *s2[i])
+		if (*s1 == *s2)
 			return (0);
 		else
 			return (*s1 - *s2);
