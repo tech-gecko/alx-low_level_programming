b77957354f52d8533a9124fc4a6b8e82a0e6c6b3
tech-gecko <w0nderzz2001@gmail.com>
2024-05-04 08:31:45 +0000

    Update #6, task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
-				 _putchar(j + '0');
-			else
-			{
+			if (j > 9)
+			{	
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
