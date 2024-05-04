c7e140d0eecc6f6205582541e9c0faa7e2c4eff6
tech-gecko <w0nderzz2001@gmail.com>
2024-05-04 08:50:52 +0000

    Update #3, tasks 8 and 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; 1 < 100, i++)
+	for (i = 0; 1 <= 100, i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else
 			printf("%d", i);
 	}
+	printf("\n");
 	return (0);
 }
