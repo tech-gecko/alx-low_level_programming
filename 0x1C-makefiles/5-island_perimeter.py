ce6e91bfe1e916201bace6918d2ba762332c5048
tech-gecko <w0nderzz2001@gmail.com>
2024-05-30 16:49:38 +0000

    Update #1, task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-	"""
-	Module to calculate the perimeter of an island in a grid.
-	"""
+"""
+Module to calculate the perimeter of an island in a grid.
+"""
 
 def island_perimeter(grid)
 	"""
