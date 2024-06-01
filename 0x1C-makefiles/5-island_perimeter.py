9768ee9fdda0f656987d75e4def295ede892d003
tech-gecko <w0nderzz2001@gmail.com>
2024-06-01 12:47:09 +0000

    Update task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,28 +5,28 @@ Module to calculate the perimeter of an island in a grid.
 
 
 def island_perimeter(grid):
-	"""
-	Calculate the perimeter of an island described in grid.
+    """
+    Calculate the perimeter of an island described in grid.
 
-	Args:
-		grid (list of list of int):
-			2D list representing the grid where 0 is water
-			and 1 is land.
+    Args:
+        grid (list of list of int):
+            2D list representing the grid where 0 is water
+            and 1 is land.
 
-	Returns:
-		int: The perimeter of the island.
-	"""
+    Returns:
+        int: The perimeter of the island.
+    """
 
-	rows = len(grid)
-	cols = len(grid[0])
-	perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
 
-	for r in range(rows):
-		for c in range(cols):
-			if grid[r][c] == 1:
-				perimeter += 4
-				if r > 0 and grid[r - 1][c] == 1:
-					perimeter -= 2
-				if c > 0 and grid[r][c - 1] == 1:
-					perimeter -= 2
-	return perimeter
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 2
+                if c > 0 and grid[r][c - 1] == 1:
+                    perimeter -= 2
+    return perimeter
