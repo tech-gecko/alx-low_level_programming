a25718778928e44d684e7361f60200d78c58a50d
tech-gecko <w0nderzz2001@gmail.com>
2024-07-16 11:49:39 -0700

    Followed all requirements, including C90 and flags.

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,17 +12,19 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
-		else
+
+		if (i == size - 1)
 			return (-1);
 	}
+	return (0);
 }
